15320995a2d454f7d637b06f3d0b993952855254
patricey0 <patrice.yolle@gmail.com>
2022-03-08 09:47:16 +0100

    added some comments

--- app/controllers/gameController.js
+++ app/controllers/gameController.js
@@ -4,8 +4,10 @@ const fetch = require('node-fetch');
 const gameController = {
 
     getRandomCommunes: async (_, res) => {
+        //fetching external api to get some cities with population
         await fetch('https://geo.api.gouv.fr/communes?codeRegion=24&fields=nom,codesPostaux,population&format=json&geometry=centre')
             .then(response => response.json())
+            //using data to return a json with 4 random cities
             .then(data => {
                 const values = Object.values(data)
                 const communes = [];
@@ -13,7 +15,6 @@ const gameController = {
                     const random = values[Math.floor(Math.random() * values.length)];
                     communes.push(random);
                 }
-                
                 res.json(communes);
             })
             .catch(err => res.status(500).json(err));
